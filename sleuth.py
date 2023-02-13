6b6f34ec9e5b1ac11242421f3d4f5bfc6247e5c6
Gurney Buchanan <gurney.buchanan@gmail.com>
2023-02-13 12:43:57 -0500

    Add some comments to better annotate snapshot-sleuth for servincability.

--- sleuth.py
+++ sleuth.py
@@ -3,43 +3,53 @@ from github import Github, UnknownObjectException, GithubException
 
 if __name__ == '__main__':
 
+    # Simple parameter loading and help text without argparse for now.
     if len(sys.argv) < 4:
         print("Usage: python sleuth.py <pipeline-repo-name> <pipeline-repo-branch> <component-repo> <component-sha>")
         exit(1)
-
     org_name = os.getenv('GITHUB_ORG') if os.getenv('GITHUB_ORG') != None else 'stolostron'
     repo = sys.argv[1]
     pipeline_branch = sys.argv[2]
     component_repo = sys.argv[3]
     component_sha = sys.argv[4]
 
-    results = {}
+    # Initialize our connection to GitHub with a token from the Env var
+    #   and grab the snapashota directory
     gh = Github(os.getenv('GITHUB_TOKEN'))
     org = gh.get_organization(org_name)
     pipeline = org.get_repo(repo)
     snapshots = pipeline.get_contents('snapshots', ref=pipeline_branch)
 
-    # Filter out downstream snapshots and gitkeep and sort
+    # Filter out downstream snapshots and gitkeep and sort oldest-to-newest
     r = re.compile('manifest-([0-9]{4}-[0-9]{2}-[0-9]{2}-[0-9]{2}-[0-9]{2}-[0-9]{2})-[0-9]{1,2}\.[0-9]{1,2}\.[0-9]{1,2}\.json')
     snapshots = list(filter(lambda s: r.search(s.name), snapshots))
     snapshots.sort(key=lambda s: s.name, reverse=False)
 
+    # If there are no snapashots to parse, report that.
     if len(snapshots) < 1:
         print(f"We found no snapshots in {org}/{repo} branch {pipeline_branch}.")
         exit(0)
+    
+    # Iterate ovr all snapshots in sorted order and compile an list of components that match the component-sha
+    #   and those that don't.  Loop until we find a full match _or_ iterate through all snapshots.
     match = []
     mismatch = []
     for snapshot in snapshots:
         manifest = json.loads(snapshot.decoded_content)
+        # Check only images build from compoonent_repo
         c = [component for component in manifest if component['git-repository'] == component_repo]
         match = []
         mismatch = []
+        # Check all git shas for matches or mismatches and append as appropriate
         for component in c:
             match.append(component) if component['git-sha256'] == component_sha else mismatch.append(component)
+        # If we find a full match, we found the snapshot, report and exir happy
         if len(match) > 0 and len(mismatch) == 0:
             print(f"All components built from {component_repo} updated to {component_sha} as of {snapshot.name}.")
             exit(0)
     
+    # At this point, matches and mismatches holds the data from the latest snapshot manifest. 
+
     # If we found no matches, check git history on the components to see if we garbage collectd the target commit.
     if len(match) > 0 and len(mismatch) > 0:
         # We found a partial match, which likely means something went wrong during a publish
@@ -98,5 +108,6 @@ if __name__ == '__main__':
         print(f"We would expect this to occur if the snapshot that first contained {component_sha} for {component_repo} had its manifest garbage collected in our weekly cleanup.")
 
     else:
-        print("An error occurred, we should've found your snapshot by now!")
+        # If we hit this case it means we have 0 matches and 0 mismatches meaning we didn't have any hits
+        print(f"We didn't find a component built from the repository {component_repo} in the latest manifest, are you sure its there?")
         exit(1)
